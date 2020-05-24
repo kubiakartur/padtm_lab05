4c6a22f54fd6a2fe3f9c35f27b59f9f5ee6332e2
kubiakartur <62542438+kubiakartur@users.noreply.github.com>
2020-05-24 18:51:06 +0200

    Dodanie listy

--- app/src/main/java/com/example/moya/padtm_lab04/MainActivity.java
+++ app/src/main/java/com/example/moya/padtm_lab04/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.moya.padtm_lab04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TODO
+
+        String[] values = new String[] { "Pies", "Kot", "Koń", "Gołąb", "Kurk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Karaluch" };
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        ListView listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(this.adapter);
     }
+
+
+
+
+
+
 }
