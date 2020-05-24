b7619b1e7a10db0d645b57e76d20a122904195df
kubiakartur <62542438+kubiakartur@users.noreply.github.com>
2020-05-24 20:21:42 +0200

    18. Commit

--- app/src/main/java/Animal.java
+++ app/src/main/java/Animal.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
  */
 
 public class Animal implements Serializable {
-    private int id;
+    private int _id;
     private String gatunek;
     private String kolor;
     private float wielkosc;
@@ -23,12 +23,12 @@ public class Animal implements Serializable {
 
     @Override
     public String toString(){
-        return "Zwierze: [id=" + id + ", gatunek=" + gatunek + ", kolor=" + kolor +" wielkosc=" + wielkosc +" ]";
+        return "Zwierze: [id=" + _id + ", gatunek=" + gatunek + ", kolor=" + kolor +" wielkosc=" + wielkosc +" ]";
     }
     public String getOpis() { return opis; }
     public String getGatunek() { return gatunek; }
     public String getKolor() { return kolor; }
     public float getWielkosc() { return wielkosc; }
-    public int getId(){ return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId(){ return _id; }
+    public void setId(int id) { this._id = id; }
 }
