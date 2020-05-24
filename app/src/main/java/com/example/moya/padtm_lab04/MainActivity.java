4136cda46aca9481c92b451321ad6de7239acc63
kubiakartur <62542438+kubiakartur@users.noreply.github.com>
2020-05-24 19:16:22 +0200

    Dodanie listy

--- app/src/main/java/com/example/moya/padtm_lab04/MainActivity.java
+++ app/src/main/java/com/example/moya/padtm_lab04/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.moya.padtm_lab04;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,5 +29,27 @@ public class MainActivity extends AppCompatActivity {
         this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
         ListView listview = (ListView) findViewById(R.id.listView);
         listview.setAdapter(this.adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi){
+        Intent intencja = new Intent(this, DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
